94f27a88adbc3505ff36b227aa47ad16ef505ba3
Aiemanjsk <aiemanmatlin9997@gmail.com>
2026-01-05 13:08:08 +0800

    lab 3

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,2 +1,10 @@
 def count_bright_spots(pixels):
-    pass
+    bright_spots = 0
+    for count in range (1, len(pixels) - 1): 
+        if pixels[count] > pixels[count - 1] and pixels[count] > pixels[count + 1]:
+            bright_spots += 1
+
+    return bright_spots
+
+pixels = [100, 120, 200, 150, 180, 160, 140]
+pass
--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,22 @@
 def find_station(stations, name):
-    pass
+    for i in range(len(stations)):
+        if stations[i] == name:
+            return i
+    return -1
+pass
 
 def count_stops(stations, start, stop):
+    for i in range(len(stations)):
+        if stations[i] == start:
+            for j in range(len(stations)):
+                if stations[j] == stop:
+                    if j > i:
+                        return (j - i)
+                    else:
+                        return (i - j)
+            return -1
+    return -1 
     pass
+
+stations = ["Central", "Marina", "Bukit", "Orchard", "Sentosa"]
+
