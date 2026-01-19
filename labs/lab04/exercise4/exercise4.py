7f47e0ae403dd3efe09bce190e8ce4c256518411
Aiemanjsk <aiemanmatlin9997@gmail.com>
2026-01-19 13:13:13 +0800

    lab5

--- labs/lab04/exercise4/exercise4.py
+++ labs/lab04/exercise4/exercise4.py
@@ -1,10 +1,28 @@
 
 def analyze_performance(lap_times):
+
+    total_first_half = 0
+    total_second_half = 0
+    if len(lap_times) % 2 == 0:
+        first_half = len(lap_times) // 2 
+    else:
+        first_half = len(lap_times) // 2 + 1
+    for i in range(first_half):
+        total_first_half += lap_times[i]
+    average_first_half = total_first_half / (len(lap_times)/2)
+    for j in range(first_half ,len(lap_times)):
+        total_second_half += lap_times[j]
+    average_second_half = total_second_half / (len(lap_times)/2)
+
+    if average_first_half < average_second_half:
+        return True
+    else:
+        return False
   
     pass
 
 
 # Test
-laps = [60, 62, 61, 63, 65, 68, 70, 72]
+laps = [50, 55, 60, 65, 70]
 result = analyze_performance(laps)
 print(f"Faded: {result}")  # Expected: True
--- labs/lab05/exercise4/exercise4.py
+++ labs/lab05/exercise4/exercise4.py
@@ -3,6 +3,26 @@ def filter_query_times(times):
     """
     Remove slow outliers (mean + std deviation) and return sorted times.
     """
+    total_first_half = 0
+    total_second_half = 0
+    if len(times) % 2 == 0:
+        first_half = len(times) // 2 
+    else:
+        first_half = len(times) // 2 + 1
+    for i in range(first_half):
+        total_first_half += times[i]
+    average_first_half = total_first_half / (len(times)/2)
+    for j in range(first_half ,len(times)):
+        total_second_half += times[j]
+    average_second_half = total_second_half / (len(times)/2)
+
+    if average_first_half < average_second_half:
+        return True
+    else:
+        return False
+    
+        
+
     pass
 
 
